ccf715b891aa170f21bde16af093880228e62bdd
Sahil Sonawane <sonawanesahil360@gmail.com>
2025-01-13 14:01:26 +0530

    payment file change

--- src/controllers/payment.ts
+++ src/controllers/payment.ts
@@ -6,22 +6,22 @@ import ErrorHandler from "../utils/utilty-class.js";
 
 
 
-export const createPaymentIntent = TryCatch(async(req, res, next) => {
+// export const createPaymentIntent = TryCatch(async(req, res, next) => {
     
-    const {amount} = req.body;
+//     const {amount} = req.body;
 
-    if(!amount) return next(new ErrorHandler("Please Enter Amount", 400));
+//     if(!amount) return next(new ErrorHandler("Please Enter Amount", 400));
 
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount : Number(amount) * 100,
-        currency : "inr",
-    });
+//     const paymentIntent = await stripe.paymentIntents.create({
+//         amount : Number(amount) * 100,
+//         currency : "inr",
+//     });
 
-    return res.status(201).json({
-        success:true,
-        clientSecret:paymentIntent.client_secret,
-    });
-});
+//     return res.status(201).json({
+//         success:true,
+//         clientSecret:paymentIntent.client_secret,
+//     });
+// });
 
 
 // controller function for creating newCoupon
